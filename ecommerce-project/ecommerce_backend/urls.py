52f2bde7ce3ba52ca2ac5ada03f932d5235b7f1e
poojareddy1106 <your-email@example.com>
2025-10-27 15:45:24 +0530

    resolve api error

--- ecommerce-project/ecommerce_backend/urls.py
+++ ecommerce-project/ecommerce_backend/urls.py
@@ -76,7 +76,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.views.generic import RedirectView
 
 @api_view(['GET'])
 def api_root(request):
@@ -102,7 +101,6 @@ urlpatterns = [
     path('api/', include('orders.urls')),
     path('api/payments/', include('payments.urls')),
     path('api/', api_root, name='api-root'),  # Add this line for /api/
-    path('', RedirectView.as_view(url='/api/', permanent=False)),
 ]
 
 if settings.DEBUG:
